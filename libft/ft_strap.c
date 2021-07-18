a2e113ff3ac65a33495f6ab15d566726c967ca4a
Jinukim <jinukim@Jinukimui-MacBookAir.local>
2021-07-18 17:25:42 +0900

    .

--- libft/ft_strap.c
+++ libft/ft_strap.c
@@ -15,12 +15,14 @@ char	**ft_strap(char **strs)
 		j = -1;
 		k = 0;
 		while (strs[i][++j])
+		{
 			if (!quot && (strs[i][j] == '\'' || strs[i][j] == '\"'))
 				quot = strs[i][j];
 			else if (quot && strs[i][j] == quot)
 				quot = 0;
 			else
 				buf[k++] = strs[i][j];
+		}
 		buf[k] = 0;
 		free(strs[i]);
 		strs[i] = ft_strdup(buf);
