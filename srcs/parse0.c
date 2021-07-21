6b17cc74de1707e53add1848ba851dadf45d1bbb
1226kjw <1226kjw@github.com>
2021-07-21 19:25:42 +0900

    .

--- srcs/parse0.c
+++ srcs/parse0.c
@@ -18,58 +18,109 @@ static char	*get_key(char *line, int *i)
 {
 	char	buf[128];
 	int		k;
+	int		flag;
 
 	k = 0;
+	flag = 0;
 	if (line[*i] == '$')
 		++*i;
 	if (line[*i] == '{')
+	{
 		++*i;
+		flag = 1;
+	}
 	while (ft_isalpha(line[*i]) || ft_isdigit(line[*i]) || line[*i] == '_')
 		buf[k++] = line[(*i)++];
-	if (line[*i] == '}')
+	if (flag && line[*i] == '}')
+	{
 		++*i;
+		flag--;
+	}
+	if (flag)
+		return (0);
 	buf[k] = 0;
 	return (ft_strdup(buf));
 }
 
-int	parse_var_0(t_parse *p, char *buf, char *line, int red)
+char	*set_val(t_parse *p, char *line)
 {
 	char	*key;
 	char	*val;
 
-	if (!red)
+	if (line[p->j + 1] == '?')
 	{
-		if (line[p->j + 1] == '?')
-		{
-			val = ft_itoa(g_ret);
-			p->j += 2;
-		}
+		val = ft_itoa(g_ret);
+		p->j += 2;
+	}
+	else if (line[p->j + 1] == '_'
+		|| ft_isalnum(line[p->j + 1]) || line[p->j + 1] == '{')
+	{
+		key = get_key(line, &p->j);
+		if (!key)
+			val = 0;
 		else
-		{
-			key = get_key(line, &p->j);
 			val = find_value(p->envl, key);
-			free(key);
-		}
+		free(key);
+	}
+	else if (!p->quot && (line[p->j + 1] == '\'' || line[p->j + 1] == '\"'))
+	{
+		val = ft_strdup("");
+		p->j++;
+	}
+	else
+	{
+		val = ft_strdup("$");
+		p->j++;
+	}
+	return (val);
+}
+
+int	parse_var_0(t_parse *p, char *buf, char *line, int *red)
+{
+	char	*val;
+
+	if (!*red)
+	{
+		val = set_val(p, line);
 		p->k = -1;
+		if (!val)
+			return (1);
 		while (val[++p->k])
 			buf[p->i++] = val[p->k];
 		free(val);
 	}
 	else
 		buf[p->i++] = line[p->j++];
+	*red = 0;
 	return (0);
 }
 
-void	parse_var_3(t_parse *p, char *buf)
+int	parse_var_3(t_parse *p, char *buf, char *line, int *red)
 {
 	char	*val;
 
-	val = find_value(p->envl, "HOME");
-	p->k = -1;
-	while (val[++p->k])
-		buf[p->i++] = val[p->k];
-	free(val);
-	p->j += 1;
+	if (!p->quot && line[p->j] == '~' && (line[p->j + 1] == 0
+			|| line[p->j + 1] == ' ' || line[p->j + 1] == '/'))
+	{
+		val = find_value(p->envl, "HOME");
+		p->k = -1;
+		while (val[++p->k])
+			buf[p->i++] = val[p->k];
+		free(val);
+		p->j += 1;
+	}
+	if (p->quot != '\'' && line[p->j] == '$')
+	{
+		if (parse_var_0(p, buf, line, red))
+		{
+			printf("minishell: syntax error, missing '}'\n");
+			buf[0] = 0;
+			return (1);
+		}
+	}
+	else
+		buf[p->i++] = line[p->j++];
+	return (0);
 }
 
 void	parse_var(char *buf, char *line, t_list *envl)
@@ -88,13 +139,8 @@ void	parse_var(char *buf, char *line, t_list *envl)
 			p->quot = 0;
 		if (line[p->j] == '<' || line[p->j] == '>')
 			red = 1;
-		if (!p->quot && line[p->j] == '~' && (line[p->j + 1] == 0
-				|| line[p->j + 1] == ' ' || line[p->j + 1] == '/'))
-			parse_var_3(p, buf);
-		if (p->quot != '\'' && line[p->j] == '$')
-			red = parse_var_0(p, buf, line, red);
-		else
-			buf[p->i++] = line[p->j++];
+		if (parse_var_3(p, buf, line, &red))
+			break ;
 	}
 	buf[p->i] = 0;
 	free(p);
